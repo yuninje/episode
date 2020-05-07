aedb0f0d9c35cb4c1558095560290f21eb0a5e0f
김동훈 <kdh4769@naver.com>
2020-05-07 09:35:11 +0900

    develop 브랜치 생성

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL : 'http://52.79.177.68:5000/api/v1',
-    baseURL : 'http://localhost:5000/api/v1',
+    baseURL : 'http://localhost:8080/api/',
     headers:{
         'Content-type':'application/json',
         // 'Authorization':'Bearer ' + sessionStorage.getItem('accessToken')
