202548c0ab0a189226ed206eaa97510571509d33
sinoni <seouland2@hanmail.net>
2020-05-11 15:01:09 +0900

    S02P31A206-21 | To Do | 5/11 박신원 에디터페이지

---
+++ frontend/src/components/editor/PageEditor.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="editor">
+    에디터 페이지 입니다.
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
