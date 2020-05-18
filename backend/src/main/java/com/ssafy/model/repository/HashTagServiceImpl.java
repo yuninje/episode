320faa5168f5d6b55c3cc88e98a135c60480f79c
KMG <sdm6067@naver.com>
2020-05-18 15:51:25 +0900

    S02P31A206-72 | In progress | Implement hashtag api

---
+++ backend/src/main/java/com/ssafy/model/repository/HashTagServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ssafy.model.repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ssafy.model.dto.HashTagDTO;
+import com.ssafy.model.entity.HashTag;
+import com.ssafy.model.service.HashTagService;
+
+public class HashTagServiceImpl implements HashTagService {
+	@Autowired
+	HashTagRepository hRepo;
+	@Autowired
+	ModelMapper modelMapper;
+	
+	@Override
+	public void registHashTag(HashTagDTO hashTag) {
+		hRepo.save(modelMapper.map(hashTag, HashTag.class));
+	}
+
+	@Override
+	public List<HashTagDTO> getHashTags() {
+		List<HashTag> results = hRepo.findAll();
+		List<HashTagDTO> hashTags = 
+				results.stream().map(hashTag -> modelMapper.map(hashTag, HashTagDTO.class))
+				.collect(Collectors.toList());
+		
+		return hashTags;
+	}
+
+	@Override
+	public List<HashTagDTO> getHashTagsByNovel(int novelPk) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
