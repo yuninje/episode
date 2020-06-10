747c78d66352d7f78a89acf5543e22c48e8a2831
Baek <chang1428@naver.com>
2020-06-10 22:26:24 +0900

    [Fix] novel setting character color

--- frontend/src/components/character/CharacterCardVerticalList.vue
+++ frontend/src/components/character/CharacterCardVerticalList.vue
@@ -10,6 +10,7 @@
         :role="char.characterRole"
         :significant="char.characterSignificant"
         :src="char.characterImage"
+        :color="getRandomRgb(char.characterName)"
       />
     </v-list>
   </div>
@@ -46,6 +47,20 @@ export default {
   methods: {
     handleResize() {
       this.window.height = window.innerHeight;
+    },
+    djb2(str){
+      let hash = 5381;
+      for (let i = 0; i < str.length; i++) {
+        hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
+      }
+      return hash;
+    }
+    ,getRandomRgb(str) {
+      let hash = this.djb2(str);
+      let r = (hash & 0xFF0000) >> 16;
+      let g = (hash & 0x00FF00) >> 8;
+      let b = hash & 0x0000FF;
+      return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
     }
   }
 };
