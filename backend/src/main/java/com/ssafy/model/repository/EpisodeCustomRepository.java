655debe63376ef197e66acd2b320983753bace92
KMG <sdm6067@naver.com>
2020-06-15 13:50:49 +0900

    modify get episode -> contains pre and next episode pk

--- backend/src/main/java/com/ssafy/model/repository/EpisodeCustomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.model.repository;
-
-import com.ssafy.model.entity.Episode;
-
-public interface EpisodeCustomRepository {
-	Episode[] findEpisodesByEpisodePk(int episodePk, int novelPk);
-}
